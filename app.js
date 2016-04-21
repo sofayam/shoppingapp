4e1f4182ea0e6585b252082469ff04fe5d4f0332
Mark Andrew <mark.andrew@de.bosch.com>
2016-04-21 16:26:56 +0200

    more drag and drop fun

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var hbs = require('hbs');
+
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var drag = require('./routes/drag');
 
 var app = express();
+
+hbs.registerHelper('times', function(n, block) {
+    var accum = '';
+    for(var i = 0; i < n; ++i)
+        accum += block.fn(i);
+    return accum;
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
