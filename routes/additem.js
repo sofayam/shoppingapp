fa471c050883d78e5f52716f43f9bf736ac69cec
Mark Andrew <mark.andrew@de.bosch.com>
2016-04-28 12:35:52 +0200

    Slightly refactored locally working additem and listitems

--- routes/additem.js
+++ routes/additem.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var request = require('request');
-var HttpsProxyAgent = require('https-proxy-agent');
-var uuid = require('uuid4');
-var agent = new HttpsProxyAgent('http://127.0.0.1:3128');
 
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+var uuid = require('uuid4');
+
+var params = require('./restparams');
 
 var router = express.Router();
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 router.get('/', function(req, res, next) {
     var thisUuid = uuid();
@@ -14,26 +15,17 @@ router.get('/', function(req, res, next) {
     console.log("uuid", thisUuid);
     res.render('additem', {title: 'Lets Go Shopping', item: req.query.item})
     if (req.query.item) {
-	request({
+ 
+	params.uri =  "https://cr.apps.bosch-iot-cloud.com/cr/1/things/markandrew:" + thisUuid;
+	params.method = "PUT";
+	params.json = {attributes: {type: "purchase", item: req.query.item}};
 
-	    uri: "https://cr.apps.bosch-iot-cloud.com/cr/1/things/markandrew:" + thisUuid, 
-	    auth: { user: "markandrew",
-		    password: "markandrewPw1!" },
-	    method: "PUT",
-	    json: {attributes: {type: "purchase", item: req.query.item}},
-	    headers: {
-		"Accept": "application/json",
-		"Content-Type": "application/json",
-		"x-cr-api-token": "6af616e401e24aa98425b825da995a7a"
-	    },
-	    agent: agent,
-	    timeout: 1000,
-	    followRedirect: true,
-	    maxRedirects: 10}, 
-		function(error, response, body) {
-		    console.log(body);
-		}
-	       )
+	request(
+	    params,
+	    function(error, response, body) {
+		console.log(body);
+	    }
+	)
     }
 });
 
--- routes/listitems.js
+++ routes/listitems.js
@@ -1,43 +1,37 @@
 var express = require('express');
 var request = require('request');
-var HttpsProxyAgent = require('https-proxy-agent');
 
-var proxyurl = 'http://127.0.0.1:3128'
-var agent = new HttpsProxyAgent(proxyurl);
+var params = require('./restparams');
 
-
-
-var params = {
-	uri: "https://cr.apps.bosch-iot-cloud.com/cr/1/search/things?filter=and(exists(attributes/item),eq(attributes/type,\"purchase\"))", 
-	auth: { user: "markandrew",
-		password: "markandrewPw1!" },
-	method: "GET",
-	headers: {
-	    "Accept": "application/json",
-	    "Content-Type": "application/json",
-	    "x-cr-api-token": "6af616e401e24aa98425b825da995a7a"
-	},
-	timeout: 1000,
-	followRedirect: true,
-	maxRedirects: 10};
-
-if (process.env.VCAP_SERVICES) {
-   console.log("No agent needed");
-} else {
-   console.log("Set agent");
-   params.agent = agent;
-}; 
-
-
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0"; 
+// following rest call will fail if this is not directly assigned in 
+// this file (will not work if just in restparams.js)
 
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
 
+    params.uri = "https://cr.apps.bosch-iot-cloud.com/cr/1/search/things?filter=and(exists(attributes/item),eq(attributes/type,\"purchase\"))";
+    params.method = "GET";
+
     request(params, 
 	    function(error, response, body) {
-		var bodydata = JSON.parse(body);
+		//console.log("response: ", response);
+		//console.log("error: ", error);
+		//console.log("type of body: ", typeof body); 
+
+		//console.log("Body : ", body);
+		try {
+		    // TBD complete mystery, sometimes the body arrives preparsed, 
+		    // sometimes as a string
+		    var bodydata = body;
+		    if (typeof body == "string") {
+			bodydata = JSON.parse(body);
+		    } 
+		} catch(e) {
+		    return console.error(e);
+		}
+		//console.log("bodydata : ", bodydata);
 		var items = bodydata.items;
 		res.render('listitems', {title: 'Here are your items', mydata: items})
 	    }
