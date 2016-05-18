00de323d02a53bbdcd34fd5df861e08b339a8657
Mark Andrew <mark.andrew@de.bosch.com>
2016-05-18 17:51:49 +0200

    the horror!

--- app.js
+++ app.js
@@ -88,6 +88,4 @@ app.use(function(err, req, res, next) {
 });
 
 
-
-
 module.exports = app;
--- routes/alloc.js
+++ routes/alloc.js
@@ -5,10 +5,49 @@ var model = require('../model/model.js');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
+
+    // build a structure with a key for each store and a list of items allocated
+
+    function getStoreIds(stores) {
+	var res = {};
+	for (var i=0; i < stores.length; i++) {
+	    res[stores[i].thingId] = 1;
+	}
+	return res
+    }
+	
+    var storesWithItems = {}
+    var remainingItems = []
+    var stores = model.getStores();
+    var storeIds = getStoreIds(stores);
+    var items = model.getItems();
+
+    console.log("Stores " + JSON.stringify(stores));
+
+    for (var i=0; i < stores.length; i++) {
+	storeId = stores[i].thingId;
+	storesWithItems[storeId] = {id: storeId, name: stores[i].attributes.name, items: []};
+    }
+    console.log("Stores with items: " + JSON.stringify(storesWithItems));
+
+    for (var i=0; i < items.length; i++) {
+	item = items[i];
+//	if ("store" in item.attributes) {
+	    if (item.attributes.store in storeIds) { 
+		// TBD what about orphan items?
+		storesWithItems[item.attributes.store].items.push(item);
+	    } else {
+		remainingItems.push(item);
+	    }
+//	}
+    }
+    
+    console.log("Stores with items: " + JSON.stringify(storesWithItems));
+
     res.render('alloc', 
 	       {title: "Allocate your shopping", 
-		mydata: model.getItems(),
-		stores: model.getStores()
+		mydata: remainingItems,
+		stores: storesWithItems
 	       })
 })
 
