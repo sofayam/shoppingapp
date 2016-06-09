57135c9866d302f92b1c1d7f20f97140d6fabf18
Mark Andrew <mark.andrew@gmail.com>
2016-06-09 20:25:33 +0200

    added some local type info

--- util/tagformatter.js
+++ util/tagformatter.js
@@ -1,10 +1,14 @@
 // Dirty little hack because en|decodeURIComponent 
 // produces ids which will not work as tags with JQuery UI drag and drop magic on alloc page
-
+/**
+ * @return {string}
+ */
 exports.decode = function(tag) {
     return tag.replace("Z",':')
 }
-
+/**
+ * @return {string}
+ */
 exports.encode = function(id) {
     return id.replace(':','Z')
 }
