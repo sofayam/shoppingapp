4f022df29a9431b508ad2b0e0fa3266ba6fdb02d
Mark Andrew <mark.andrew@gmail.com>
2016-06-12 12:38:56 +0200

    first attempt with type annotations for js

--- util/tagformatter.js
+++ util/tagformatter.js
@@ -1,12 +1,14 @@
 // Dirty little hack because en|decodeURIComponent 
 // produces ids which will not work as tags with JQuery UI drag and drop magic on alloc page
 /**
+ * @param {string} tag
  * @return {string}
  */
 exports.decode = function(tag) {
     return tag.replace("Z",':')
 }
 /**
+ * @param {string} id
  * @return {string}
  */
 exports.encode = function(id) {
