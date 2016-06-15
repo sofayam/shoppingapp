f28eaf0613b0316b73e9a53e4704f11cbddf4305
Mark Andrew <mark.andrew@de.bosch.com>
2016-06-15 12:50:27 +0200

    warning if suspected cntlm forgotten

--- routes/config.js
+++ routes/config.js
@@ -1,15 +1,22 @@
 var express = require('express');
 var configfile = require('./configfile');
+var model = require('../model/model.js');
 
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
     var config = configfile.getConfig();
     console.log("config ", config);
-    res.render('config', {
-        title: 'Configure the server (experts only please!)',
-        color: configfile.getSelected("colour"),
-        config: config
+    var title = 'Configure the server (experts only please!)';
+    model.getThings(function (things) {
+        if (things.length == 0) {
+            title = "Warning: no things detected, do you have your cntlm proxy started?"
+        }
+        res.render('config', {
+            title: title,
+            color: configfile.getSelected("colour"),
+            config: config
+        })
     })
 })
 
