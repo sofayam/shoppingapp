c99d4cc2c55dce7c1afd2a87a2d71148c488e452
Mark Andrew <mark.andrew@de.bosch.com>
2016-06-27 17:19:38 +0200

    only use crmodel on BICS

--- model/model.js
+++ model/model.js
@@ -1,24 +1,28 @@
-configfile = require ('../routes/configfile.js');
+configfile = require('../routes/configfile.js');
 
-var model = configfile.getSelected("model");
-
-var requirePath = "./" + model;
-
+var requirepath = "";
+if (process.env.VCAP_SERVICES) { // always use Things if you are running on the BICS
+	requirePath = "./crmodel";
+} else {
+	var model = configfile.getSelected("model");
+	requirePath = "./" + model;
+}
 var res = require(requirePath);
+
 console.log("requiring ", requirePath);
 module.exports = exports = res
 
 // TBD this is the first one which is identical for both models, 
 // maybe do something with prototypes to share this code properly
-module.exports.cancelRemoveTrip = function(storeId) {
+module.exports.cancelRemoveTrip = function (storeId) {
     console.log("cancel and remove storeid from all things", storeId);
     exports.setTripDate(storeId, "unset");
-    exports.getItemsForStore(storeId, function(items) {
-	for(var i = 0; i < items.length; i++) {
-	    var itemId = items[i].thingId;
-	    console.log("clearing item " , i, itemId)
-	    exports.clearStoreForItem(itemId);
-	}
+    exports.getItemsForStore(storeId, function (items) {
+		for (var i = 0; i < items.length; i++) {
+			var itemId = items[i].thingId;
+			console.log("clearing item ", i, itemId)
+			exports.clearStoreForItem(itemId);
+		}
     })
 }
 
